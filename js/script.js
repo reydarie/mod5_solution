7d42d9c5c3383b0f561cf305661d5831f8312025
Rey <dario-edgardo.rey@capgemini.com>
2020-06-11 19:16:48 -0300

    fix

--- js/script.js
+++ js/script.js
@@ -103,7 +103,7 @@ $(function () {
         // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
         // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
         // variable's name implies it expects.
-        var chosenCategoryShortName = chooseRandomCategory(categories);
+        var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
         // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
         // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -115,11 +115,11 @@ $(function () {
         // $dc.loadMenuItems('L')
         // Hint: you need to surround the chosen category short name with something before inserting
         // it into the home html snippet.
-
+         
         var homeHtmlToInsertIntoMainPage = insertProperty(
           homeHtml,
           "randomCategoryShortName",
-          chosenCategoryShortName.short_name
+          "'" + chosenCategoryShortName + "'"
         );
 
         // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
